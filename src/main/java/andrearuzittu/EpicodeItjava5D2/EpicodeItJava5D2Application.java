a354416cb16f06cabd458f0fd272305062294ef8
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-08-01 15:02:33 +0200

    upDate

---
+++ src/main/java/andrearuzittu/EpicodeItjava5D2/EpicodeItJava5D2Application.java
@@ -0,0 +1,13 @@
+package andrearuzittu.EpicodeItjava5D2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeItJava5D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeItJava5D2Application.class, args);
+	}
+
+}
---
+++ src/test/java/andrearuzittu/EpicodeItjava5D2/EpicodeItJava5D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package andrearuzittu.EpicodeItjava5D2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeItJava5D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
